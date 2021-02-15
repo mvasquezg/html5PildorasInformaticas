c92e148195b2b0389b85efec1464f1389ef5c960
mvga <ing.mvasquezg@gmail.com>
2021-02-15 13:52:25 -0600

    Creando un programa y directorios de archivos investigar este tema para poder reolver la practica.

---
+++ js/creandoArchivos.js
@@ -0,0 +1,115 @@
+
+/*Creando archivos
+function comenzar(){    
+    idZonaDatos=document.getElementById("zonadatos");
+    idBtnBoton=document.getElementById("boton");
+
+    idBtnBoton.addEventListener("click", crear, false);
+
+    navigator.webkitPersistentStorage.requestQuota(5*1024*1024, acceder);
+}
+
+function crear(){
+
+}//end crear
+
+function acceder(){
+    window.webkitRequestFileSystem(PERSISTENT, 5*1024*1024, crearSis, mostrarErrores);
+}//end acceder
+
+function crearSis(sistema){
+    espacio=sistema.root;
+}
+
+function crear(){
+    var nombreArchivo=document.getElementById("entrada").value;
+
+    if( nombreArchivo !=""){
+        espacio.getFile(nombreArchivo, {create:true, exclusive:false}, mostrar, mostrarErrores);
+    }
+}
+
+function mostrar(entrada){
+    document.getElementById("entrada").value="";
+
+    idZonaDatos.innerHTML="Exito en la creacion de espacio y archivo!<p></p>";
+    
+    idZonaDatos.innerHTML+="Nombre archivo: "+  entrada.name+"<p></p>"
+    idZonaDatos.innerHTML+="Ruta: "+  entrada.fullPath+"<p></p>"
+
+}//end mostrar 
+
+function mostrarErrores(e){
+    alert("Ha habido un error: "+ e.code);
+}*/
+
+/******************Creando directorios************************/
+function comenzar(){    
+    idZonaDatos=document.getElementById("zonadatos");
+    idBtnBoton=document.getElementById("boton");
+
+    idBtnBoton.addEventListener("click", crear, false);
+
+    navigator.webkitPersistentStorage.requestQuota(5*1024*1024, acceder);
+}
+
+function acceder(){
+    window.webkitRequestFileSystem(PERSISTENT, 5*1024*1024, crearSis, mostrarErrores);
+}//end acceder
+
+function crearSis(sistema){
+    espacio=sistema.root;
+    ruta="";
+    mostrar();
+}
+
+function crear(){
+    var nombreArchivo=document.getElementById("entrada").value;
+
+    if( nombreArchivo != ""){
+          nombreArchivo=ruta+nombreArchivo;  
+        espacio.getFile(nombreArchivo, {create:true, exclusive:false}, mostrar, mostrarErrores);
+    }
+
+}//end crear
+
+function mostrar(){
+    document.getElementById("entrada").value="";
+    idZonaDatos="";
+
+    espacio.getDirectory(ruta, null, leerDir, mostrarErrores);
+
+
+}//end mostrar
+
+
+function leerDir(dir){
+    lector=dir.createReader();
+
+    leer;
+}
+
+function leer(){
+    lector.readEntries(function(archivos){
+        if(archivos.length){
+            listar(archivos);
+        }
+
+    }, mostrarErrores);
+}
+
+function listar(archivos){
+    for(var i=0; i<archivos.length; i++){
+        if(archivos[i].isFile){
+            idZonaDatos.innerHTML+=archivos[i].name+"<p></p>"
+        }else if(archivos[i].isDirectory){
+            idZonaDatos.innerHTML+="<span class='directorio'>" + archivos[i].name + "</span><p></p>"
+        }
+    }
+}
+
+function mostrarErrores(e){
+    alert("Ha habido un error: " + e.code);
+}   
+
+window.addEventListener("load", comenzar, false);
